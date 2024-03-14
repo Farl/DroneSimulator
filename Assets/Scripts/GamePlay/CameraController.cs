b580b937ccb4af130723cdff420b47a867084106
DanielTkachenko <pdg.danila.98@mail.ru>
2024-03-15 01:44:41 +0700

    Add camera rotation and zooming

---
+++ Assets/Scripts/GamePlay/CameraController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using UnityEngine;
+
+namespace Drone.Scripts.GamePlay
+{
+    public class CameraController : MonoBehaviour
+    {
+        [SerializeField] private InputManager _inputManager;
+        [Header("Rotation Angels")]
+        [SerializeField] private float minHorizontalAngel;
+        [SerializeField] private float maxHorizontalAngel;
+        [SerializeField] private float minVerticalAngel;
+        [SerializeField] private float maxVerticalAngel;
+        [Header("Rotation Speed")]
+        [SerializeField] private float horizontalSpeed;
+        [SerializeField] private float verticalSpeed;
+
+        private void Update()
+        {
+            if(_inputManager.ActionMap != ActionMap.Camera)
+                return;
+            ProcessCameraRotations();
+        }
+
+        private void ProcessCameraRotations()
+        {
+            UpdateRotation();
+            UpdateZoom();
+        }
+
+        private void UpdateRotation()
+        {
+            var hAngel = transform.rotation.eulerAngles.y 
+                         + (horizontalSpeed * _inputManager.RightLeftRotation * Time.deltaTime);
+
+            hAngel = hAngel > 180 ? hAngel - 360 : hAngel;
+
+            if (hAngel < minHorizontalAngel)
+            {
+                hAngel = minHorizontalAngel;
+            }
+            else if (hAngel > maxHorizontalAngel)
+            {
+                hAngel = maxHorizontalAngel;
+            }
+            
+            var vAngel = transform.rotation.eulerAngles.x 
+                         + (verticalSpeed * _inputManager.UpDownRotation * Time.deltaTime * -1);
+
+            vAngel = vAngel > 180 ? vAngel - 360 : vAngel;
+            
+            if (vAngel < minVerticalAngel)
+            {
+                vAngel = minVerticalAngel;
+            }
+            else if (vAngel > maxVerticalAngel)
+            {
+                vAngel = maxVerticalAngel;
+            }
+            
+            transform.rotation = Quaternion.Euler(vAngel, hAngel, 0);
+        }
+
+        private void UpdateZoom()
+        {
+            
+        }
+
+        
+    }
+}
--- Assets/Scripts/GamePlay/InputManager.cs
+++ Assets/Scripts/GamePlay/InputManager.cs
@@ -1,8 +1,14 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
 namespace Drone.Scripts.GamePlay
 {
+    public enum ActionMap
+    {
+        Drone,
+        Camera
+    }
     [RequireComponent(typeof(PlayerInput))]
     public class InputManager : MonoBehaviour
     {
@@ -11,19 +17,46 @@ namespace Drone.Scripts.GamePlay
         private Vector2 _cyclic;
         private float _pedals;
         private float _throttle;
+        private float _switch;
+        private float _rightLeftRotation;
+        private float _upDownRotation;
+        private float _zoom;
+
+        private PlayerInput _input;
+        private ActionMap _actionMap;
+        private float timer;
 
         public Vector2 Cyclic => _cyclic;
         public float Pedals => _pedals;
         public float Throttle => _throttle;
+        public float Switch => _switch;
+        public float RightLeftRotation => _rightLeftRotation;
+        public float UpDownRotation => _upDownRotation;
+        public float Zoom => _zoom;
+        public ActionMap ActionMap => _actionMap;
 
         #endregion
 
         #region Main Methods
 
-        // Update is called once per frame
+        private void Start()
+        {
+            _input = GetComponent<PlayerInput>();
+        }
+
         void Update()
         {
-        
+            if (_switch == 1)           //switching control modes
+            {
+                timer += Time.deltaTime;
+                if (timer >= 1.0f)
+                {
+                    _actionMap = _actionMap == ActionMap.Drone ? ActionMap.Camera : ActionMap.Drone;
+                    _input.currentActionMap = _input.actions.FindActionMap(_actionMap.ToString());
+                    timer = 0;
+                    Debug.Log(_actionMap);
+                }
+            }
         }
 
         #endregion
@@ -44,6 +77,26 @@ namespace Drone.Scripts.GamePlay
         private void OnThrottle(InputValue value)
         {
             _throttle = value.Get<float>();
+        }
+
+        private void OnSwitch(InputValue value)
+        {
+            _switch = value.Get<float>();
+        }
+
+        private void OnRightLeftRotation(InputValue value)
+        {
+            _rightLeftRotation = value.Get<float>();
+        }
+
+        private void OnUpDownRotation(InputValue value)
+        {
+            _upDownRotation = value.Get<float>();
+        }
+
+        private void OnZoom(InputValue value)
+        {
+            _zoom = value.Get<float>();
         }
         #endregion
     }
