2e779ecb84f58102c324c71647bef32010f58c4c
DanielTkachenko <pdg.danila.98@mail.ru>
2024-06-05 00:30:35 +0700

    modify scenes. reorganize files

--- Assets/Scripts/GamePlay/DeadZoneScript.cs
+++ Assets/Scripts/GamePlay/DeadZoneScript.cs
@@ -1,16 +1,16 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DangerZoneScript : MonoBehaviour
+public class DeadZoneScript : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.layer == 6)
         {
-            Debug.Log("drone in");
-            DisplayMsg.showAll("Don't get close to power lines", 2f);
+            Debug.Log("dead zone");
+            DisplayMsg.showAll("Drone is lost", 2);
+            DroneMainScript.current.isBlocked = true;
         }
     }
 
